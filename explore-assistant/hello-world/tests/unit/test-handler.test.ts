169f4342619f9c990f67c29bc2cbff5296fff735
shichisan21 <photograph.by@gmail.com>
2024-03-29 22:46:58 +0900

    Jestダメ

--- explore-assistant/hello-world/tests/unit/test-handler.test.ts
+++ explore-assistant/hello-world/tests/unit/test-handler.test.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy, JestMockExtended } from 'jest-mock-extended';
-import prisma from '../client';
+import prisma from '../../client';
+import jest from '../jest@globals';
 
 jest.mock('./client', () => ({
     __esModule: true,
     default: mockDeep<PrismaClient>(),
 }));
 
-beforeEach(() => {
+() => {
     mockReset(prismaMock);
-});
+};
 
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
