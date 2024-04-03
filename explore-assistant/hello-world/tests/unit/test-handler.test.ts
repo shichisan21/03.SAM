5109025255d9f56b36164c3fb761bd0ad6d28cc2
shichisan21 <photograph.by@gmail.com>
2024-04-03 22:34:32 +0900

    jestエラー解決

--- explore-assistant/hello-world/tests/unit/test-handler.test.ts
+++ explore-assistant/hello-world/tests/unit/test-handler.test.ts
@@ -1,14 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
 import prisma from '../../client';
+import { jest, describe, test, expect } from '@jest/globals';
 
-jest.mock('./client', () => ({
+// prisma クライアントのモックを設定
+jest.mock('../../client', () => ({
     __esModule: true,
     default: mockDeep<PrismaClient>(),
 }));
 
-() => {
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
+
+// 必要に応じてモックをリセットする関数
+export const resetPrismaMock = () => {
     mockReset(prismaMock);
 };
-
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
